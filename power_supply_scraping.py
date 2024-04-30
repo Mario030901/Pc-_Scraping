58efc16cd7b3e0111903576558e63cd14be60b98
Mario030901 <mario.campana@itsrizzoli.it>
2024-04-30 12:18:43 +0200

    complited

--- power_supply_scraping.py
+++ power_supply_scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import os
 
-def pws(psu_model: str):
+def pws(psu_model: str):   # power supply model is used to search a specified CPU model
     
     info_psu = {"name": [], "price": [], "link": []}
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
@@ -10,26 +10,31 @@ def pws(psu_model: str):
     # URL to search power supplies
     url = f"https://www.amazon.it/s?k=power+supply+corsair{psu_model}"
 
-    response = requests.get(url, headers=HEADERS)
-    soup = bs(response.content, 'html.parser')
+    response = requests.get(url, headers=HEADERS)  # makes a HTTP request to the URL
+    soup = bs(response.content, 'html.parser')  # Analyzes the response
 
     # Saving HTML content to analyze it on the machine
     current_dir = os.path.dirname(__file__)
     psu_name = f"amazon_pws_{psu_model}.html"
     file_path = os.path.join(current_dir, psu_name)
     
+     # If the file exists deletes it
     if os.path.exists(file_path):
         os.remove(file_path)
 
+    # Saving the file
     with open(file_path, 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
 
+     # Analysis of the saved HTML file  to get products infos
     with open(file_path, 'r', encoding='utf-8') as file:
         soup = bs(file, 'html.parser')
 
+    # Download of the page and info extraction for every product
     links = soup.find_all('a', attrs={'class': 'a-link-normal s-no-outline'})
     links_list = [link.get('href') for link in links[:3]]  # Limit to first 3 elements
 
+    # Download of the page and info extraction for every product
     for link in links_list:
         product_url = "https://www.amazon.it" + link
         product_page = requests.get(product_url, headers=HEADERS)
