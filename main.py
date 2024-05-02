0e90297ed5b8e479f0749b21a74ae2d277cc4152
MirkoLorenzoLaRocca <mirkolorenzo.larocca@gmail.com>
2024-05-02 14:40:20 +0200

    updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Import
+import requests  # Imports library for HTTP requests
+from bs4 import BeautifulSoup as bs  # Imports BeautifulSoup for HTML parsing
+import os  # Imports library to interact with the operating system
 import case_scraping as c
 import cpu_scraping as cpu
 import gpu_scraping as gpu
@@ -6,7 +9,7 @@ import liquid_dissipator_scraping as ld
 import motherboard_scraping as mb
 import ssd_scraping as ssd
 import ram_scraping as ram
-import power_supply_scraping as pws 
+import power_supply_scraping as pws
 # End of import
 
 print("Before we start please note that we scrape from amazon, so, if you insert a model that doesn't exist you won't get nothing. Be careful")
@@ -18,6 +21,7 @@ cpu_infos={}
 scelta = 0
 budget = 0
 preferenza = ""
+cont,cheapest_cpu, cheapest_case, cheapest_dissipator, cheapest_gpu = 0, {}, {}, {}, {}
 
 # Prima di procedere con la scelta della scheda, chiedi il budget e le preferenze dell'utente
 '''try:
@@ -57,7 +61,7 @@ while True: #this while breaks when it finds a link, or better when the inserted
 # Request of the desired Case
 print("Request of the desired Corsair Case\n")
 while True: #this while breaks when it finds a link, or better when the inserted model exists on amazon
-    print("example of attended insert: crystal 570X\n")
+    print("example of attended insert: 4000D\n")
     case_infos=c.case(input("insert the desired case:\n"))
     if case_infos["link"]!=[]:
         break
@@ -68,8 +72,8 @@ while True: #this while breaks when it finds a link, or better when the inserted
 print("Request of the desired Arctic liquid freezer\n")
 while True: #this while breaks when it finds a link, or better when the inserted model exists on amazon
     print("example of attended insert: 420\n")
-    case_infos=ld.dissipator(input("insert the desired liquid freezer model:\n"))
-    if case_infos["link"]!=[]:
+    dissipator_infos=ld.dissipator(input("insert the desired liquid freezer model:\n"))
+    if dissipator_infos["link"]!=[]:
         break
     else: 
         print("the desired model doesn't exist on amazon, try searching something else\n")
@@ -78,8 +82,182 @@ while True: #this while breaks when it finds a link, or better when the inserted
 print("Request of the desired NVIDIA GPU\n")
 while True: #this while breaks when it finds a link, or better when the inserted model exists on amazon
     print("example of attended insert: 4070 | 4070 super\n")
-    case_infos=gpu.gpu(input("insert the desired GPU model:\n"))
-    if case_infos["link"]!=[]:
+    gpu_infos=gpu.gpu(input("insert the desired GPU model:\n"))
+    if gpu_infos["link"]!=[]:
         break
     else: 
         print("the desired model doesn't exist on amazon, try searching something else\n")
+        
+print("What would you like to do now?") # asks what to do next
+while True:
+    try:
+        scelta = int(input("Download the webpage of the cheapest products (1) | Show the cheapest products (2) | Quit (3)\n")) # shows the options and waits for an answer
+    except:
+        print("insert a number")
+    else:
+        if 1<=scelta<=3: break
+        else: print("Try again")
+
+if scelta == 1: #searches the cheapest products and downloades the amazon webpage
+    print("downloading CPU file")
+    for i in zip(cpu_infos["price"], cpu_infos["link"]):
+        if cont==0:
+            cheapest_cpu["price"]=i[0]
+            cheapest_cpu["link"]=i[1]
+        else:
+            if cheapest_cpu["price"]>i[0]: 
+                cheapest_cpu["price"] = i[0]
+                cheapest_cpu["link"]=i[1]
+        cont+=1
+    cont=0
+    
+    # Saving the file
+    current_dir = os.path.dirname(__file__)
+    cpu_file = f"cheapest_cpu.html"
+    cpu_file_path = os.path.join(current_dir, cpu_file)
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'} # simulates a browser request
+    response = requests.get(cheapest_cpu["link"], headers=HEADERS) # makes a HTTP request to the URL
+    soup = bs(response.content, 'html.parser') # Analyzes the response
+    # If the file exists deletes it
+    if os.path.exists(cpu_file_path):
+        os.remove(cpu_file_path)
+    with open(cpu_file_path, 'w', encoding='utf-8') as f:
+        f.write(soup.prettify())
+
+    print("downloading CASE file")
+    for i in zip(case_infos["price"], case_infos["link"]):
+        if cont==0:
+            cheapest_case["price"]=i[0]
+            cheapest_case["link"]=i[1]
+        else:
+            if cheapest_case["price"]>i[0]: 
+                cheapest_case["price"] = i[0]
+                cheapest_case["link"]=i[1]
+        cont+=1
+    cont=0
+    
+    # Saving the file
+    current_dir = os.path.dirname(__file__)
+    case_file = f"cheapest_case.html"
+    case_file_path = os.path.join(current_dir, case_file)
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'} # simulates a browser request
+    response = requests.get(cheapest_case["link"], headers=HEADERS) # makes a HTTP request to the URL
+    soup = bs(response.content, 'html.parser') # Analyzes the response
+    # If the file exists deletes it
+    if os.path.exists(case_file_path):
+        os.remove(case_file_path)
+    with open(case_file_path, 'w', encoding='utf-8') as f:
+        f.write(soup.prettify())
+
+    print("downloading LIQUID FREEZER file")
+    for i in zip(dissipator_infos["price"], dissipator_infos["link"]):
+        if cont==0:
+            cheapest_dissipator["price"]=i[0]
+            cheapest_dissipator["link"]=i[1]
+        else:
+            if cheapest_dissipator["price"]>i[0]: 
+                cheapest_dissipator["price"] = i[0]
+                cheapest_dissipator["link"]=i[1]
+        cont+=1
+    cont=0
+    
+    # Saving the file
+    current_dir = os.path.dirname(__file__)
+    dissipator_file = f"cheapest_dissipator.html"
+    dissipator_file_path = os.path.join(current_dir, dissipator_file)
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'} # simulates a browser request
+    response = requests.get(cheapest_dissipator["link"], headers=HEADERS) # makes a HTTP request to the URL
+    soup = bs(response.content, 'html.parser') # Analyzes the response
+    # If the file exists deletes it
+    if os.path.exists(dissipator_file_path):
+        os.remove(dissipator_file_path)
+    with open(dissipator_file_path, 'w', encoding='utf-8') as f:
+        f.write(soup.prettify())
+
+    print("downloading GPU file")
+    for i in zip(gpu_infos["price"], gpu_infos["link"]):
+        if cont==0:
+            cheapest_gpu["price"]=i[0]
+            cheapest_gpu["link"]=i[1]
+        else:
+            if cheapest_gpu["price"]>i[0]: 
+                cheapest_gpu["price"] = i[0]
+                cheapest_gpu["link"]=i[1]
+        cont+=1
+    cont=0
+    
+    # Saving the file
+    current_dir = os.path.dirname(__file__)
+    gpu_file = f"cheapest_gpu.html"
+    gpu_file_path = os.path.join(current_dir, gpu_file)
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'} # simulates a browser request
+    response = requests.get(cheapest_gpu["link"], headers=HEADERS) # makes a HTTP request to the URL
+    soup = bs(response.content, 'html.parser') # Analyzes the response
+    # If the file exists deletes it
+    if os.path.exists(gpu_file_path):
+        os.remove(gpu_file_path)
+    with open(gpu_file_path, 'w', encoding='utf-8') as f:
+        f.write(soup.prettify())
+
+elif scelta==2:
+    print("searching cheaper CPU")
+    for i in zip(cpu_infos["name"], cpu_infos["price"], cpu_infos["link"]):
+        if cont==0:
+            cheapest_cpu["title"]=i[0]
+            cheapest_cpu["price"]=i[1]
+            cheapest_cpu["link"]=i[2]
+        else:
+            if cheapest_cpu["price"]>i[0]: 
+                cheapest_cpu["title"]=i[0]
+                cheapest_cpu["price"]=i[1]
+                cheapest_cpu["link"]=i[2]
+        cont+=1
+    cont=0
+    print(f"I've found it, here it is\n{cheapest_cpu}")
+    
+    print("searching cheaper CASE")
+    for i in zip(case_infos["name"], case_infos["price"], case_infos["link"]):
+        if cont==0:
+            cheapest_case["title"]=i[0]
+            cheapest_case["price"]=i[1]
+            cheapest_case["link"]=i[2]
+        else:
+            if cheapest_case["price"]>i[0]: 
+                cheapest_case["title"]=i[0]
+                cheapest_case["price"]=i[1]
+                cheapest_case["link"]=i[2]
+        cont+=1
+    cont=0
+    print(f"I've found it, here it is\n{cheapest_case}")
+    
+    print("searching cheaper LIQUID FREEZER")
+    for i in zip(dissipator_infos["name"], dissipator_infos["price"], dissipator_infos["link"]):
+        if cont==0:
+            cheapest_dissipator["title"]=i[0]
+            cheapest_dissipator["price"]=i[1]
+            cheapest_dissipator["link"]=i[2]
+        else:
+            if cheapest_dissipator["price"]>i[0]: 
+                cheapest_dissipator["title"]=i[0]
+                cheapest_dissipator["price"]=i[1]
+                cheapest_dissipator["link"]=i[2]
+        cont+=1
+    cont=0
+    print(f"I've found it, here it is\n{cheapest_dissipator}")
+    
+    print("searching cheaper GPU")
+    for i in zip(gpu_infos["name"], gpu_infos["price"], gpu_infos["link"]):
+        if cont==0:
+            cheapest_gpu["title"]=i[0]
+            cheapest_gpu["price"]=i[1]
+            cheapest_gpu["link"]=i[2]
+        else:
+            if cheapest_gpu["price"]>i[0]: 
+                cheapest_gpu["title"]=i[0]
+                cheapest_gpu["price"]=i[1]
+                cheapest_gpu["link"]=i[2]
+        cont+=1
+    cont=0
+    print(f"I've found it, here it is\n{cheapest_gpu}")
+else:
+    print("We hope you had a nice time and you've found the perfect components for your PC. We hope to see you soon!")
